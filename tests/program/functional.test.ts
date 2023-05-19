ce75afbc94f01e728c68f0ddec46293a0042818e
Dushyant Yadav <dushyant9309@gmail.com>
2023-05-19 13:43:09 +0530

    Add test for branched execution

--- tests/program/functional.test.ts
+++ tests/program/functional.test.ts
@@ -1,79 +1,38 @@
 import { assertEquals } from '../../test_deps.ts'
 import { FProgram } from '../../src/program/functional.ts'
 import { FunctionalProgramDsl } from '../../src/program/program.d.ts'
+import { stdpath } from '../../deps.ts'
 
 const functionType = '/Users/dushyant/Maya/newModules/stdlib/symbols/function/function.ts'
-// const functionType = 'gh:mayahq/stdlib/function'
 const emptyChildren = {
     in: [[]],
     out: [],
     symbols: [],
 }
 
-const dsl: FunctionalProgramDsl = {
-    symbols: [
-        {
-            id: 'f1',
-            label: 'f1',
-            type: functionType,
-            inputs: {
-                body: {
-                    type: 'string',
-                    value: 'console.log(`hey`); return { output: 1 };',
-                },
-                input: {
-                    type: 'string',
-                    value: 'bruh',
-                },
-            },
-            outputs: {},
-            children: emptyChildren,
-        },
-        {
-            id: 'f2',
-            label: 'f2',
-            type: functionType,
-            inputs: {
-                body: {
-                    type: 'string',
-                    value: 'console.log(`hehe`); return { output: input * 2 }',
-                },
-                input: {
-                    type: 'symbol',
-                    symbolId: 'f1',
-                    value: 'output',
-                },
-            },
-            outputs: {},
-            children: emptyChildren,
-        },
-        {
-            id: 'f3',
-            label: 'f3',
-            type: functionType,
-            inputs: {
-                body: {
-                    type: 'string',
-                    value: 'console.log(`hehe`); return { output: input * 2 }',
-                },
-                input: {
-                    type: 'symbol',
-                    symbolId: 'f2',
-                    value: 'output',
-                },
-            },
-            outputs: {},
-            children: emptyChildren,
-        },
-    ],
-}
+const __dirname = new URL(import.meta.url).pathname
 
 Deno.test('Functional paradigm program execution', async (t) => {
+    const dsl: FunctionalProgramDsl = JSON.parse(
+        Deno.readTextFileSync(stdpath.join(__dirname, '../data/linearFunctionalDsl.json')),
+    )
+    const simpleBranchedDsl: FunctionalProgramDsl = JSON.parse(
+        Deno.readTextFileSync(stdpath.join(__dirname, '../data/branchedFunctionalDsl.json')),
+    )
+
     await t.step('Simple linear program', async () => {
         const program = new FProgram({ dsl: dsl, rootNodeId: 'f3' })
         await program.deploy()
         const result = await program.run()
 
         assertEquals(result.output, 4)
+    })
+
+    await t.step('Simple branched program', async () => {
+        const program = new FProgram({ dsl: simpleBranchedDsl, rootNodeId: 'f3' })
+        await program.deploy()
+        const result = await program.run()
+
+        assertEquals(result.output, 6)
     })
 })
